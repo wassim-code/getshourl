a57ea7b8a360fc9fe19269474fae43c5f78f0ca7
wassim-code <ghachiwassim200@gmail.com>
2020-10-31 21:02:15 +0100

    fixed admin issue

--- core/admin.py
+++ core/admin.py
@@ -5,6 +5,6 @@ from .models import Url
 admin.site.site_header = "GetShourl Administration"
 
 class UrlAdmin(admin.ModelAdmin):
-    list_display = '__all__'
+    list_display = ['url', 'redirect_url', 'add_tm']
 
 admin.site.register(Url)
--- core/models.py
+++ core/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Url(models.Model):
-    url = models.CharField(max_length=30, unique=True, primary_key=True)
+    url = models.CharField(max_length=10, unique=True, primary_key=True)
     redirect_url = models.CharField(max_length=200)
-    add_tm = models.DateTimeField(auto_now_add=True)
+    add_tm = models.DateTimeField(auto_now_add=True, verbose_name='add time')
     total_clicks = models.IntegerField(default=0)
 
     def __str__(self):
