75d837168e12388357c953de05f711e248b78796
wassim-code <ghachiwassim200@gmail.com>
2020-10-31 21:04:50 +0100

    dfsf

--- core/admin.py
+++ core/admin.py
@@ -5,6 +5,6 @@ from .models import Url
 admin.site.site_header = "GetShourl Administration"
 
 class UrlAdmin(admin.ModelAdmin):
-    list_display = ['url', 'redirect_url', 'add_tm']
+    list_display = ['url', 'redirect_url', 'total_clicks', 'add_tm']
 
-admin.site.register(Url)
+admin.site.register(Url, UrlAdmin)
